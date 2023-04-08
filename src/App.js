ff7cb120793eb17b191792dc39dbc2069fb4fabd
Keshav7091 <keshavk1002003@gmail.com>
2023-04-08 19:06:05 +0530

    Created a Todo List Application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoList from './componets/TodoList'
+
+function App() {
+  return (
+    <div className="Todo-List-Container">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/TodoList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+export default function TodoList() {
+  // State Array which hold all TODOS
+  const [todos, setTodos] = useState([]);
+
+  // Add todo to list
+  const addTask = task => {
+    if (!task.text) {
+      return
+    }
+
+    const newTodos = [task, ...todos];
+    setTodos(newTodos);
+  };
+
+  // Remove todo from list
+  const removeTask = id => {
+    let updatedTask = [...todos].filter(task => task.id !== id)
+    setTodos(updatedTask)
+  }
+
+  // Task is completed
+  const completeTask = id => {
+    let updatedTask = todos.map(todo =>{
+      if (todo.id === id) {
+        todo.isComplete = true;
+      }
+      return todo;
+    })
+    setTodos(updatedTask)
+  }
+
+  return (
+    <div>
+      <TodoForm addTask={addTask}></TodoForm>
+      <Todo todos={todos} completeTask={completeTask} removeTask={removeTask}></Todo>
+    </div>
+  );
+}
